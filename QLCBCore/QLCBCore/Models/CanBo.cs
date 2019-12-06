6955f0bf616758e304c7fe744c4d162443e311a3
dosehieu <dosehieu@gmail.com>
2019-12-06 21:40:19 +0700

    ok

---
+++ QLCBCore/QLCBCore/Models/CanBo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLCBCore.Models
+{
+    public class CanBo
+    {
+        public string HoTen;
+    }
+}
---
+++ QLCBCore/QLCBCore/Models/QLCBDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLCBCore.Models
+{
+    public class QLCBDbContext : DbContext
+    {
+        public QLCBDbContext() : base()
+        {
+            
+        }
+        DbSet<CanBo> CanBos;
+    }
+}
