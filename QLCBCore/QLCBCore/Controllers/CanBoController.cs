68d1a62abcd381df82c50e9f6fbd8a385621d0a8
dosehieu <dosehieu@gmail.com>
2019-12-30 20:47:36 +0700

    index canbo

--- QLCBCore/QLCBCore/Controllers/CanBoController.cs
+++ QLCBCore/QLCBCore/Controllers/CanBoController.cs
@@ -22,7 +22,7 @@ namespace QLCBCore.Controllers
         // GET: CanBo
         public async Task<IActionResult> Index()
         {
-            
+            var i = 1;
             return View(await _context.CanBos.Include(n => n.dmChucVu).Include(n => n.dmDonVi).Include(n => n.dmKieuCanBo).Include(n => n.dmTrinhDo).ToListAsync());
         }
 
